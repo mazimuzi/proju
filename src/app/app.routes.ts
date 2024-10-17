94886cd8fb5084201ccad03a0a0f48ed14512478
mazimuzi <eemelisalmikunnas@gmail.com>
2024-10-17 11:08:10 +0300

    ??

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 export const routes: Routes = [
+    { path: 'hello', component: HelloWorldComponent },
     { path: '**', component: PageNotFoundComponent }
 ];
---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-hello-world',
+  standalone: true,
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css'
+})
+export class HelloWorldComponent {
+
+}
