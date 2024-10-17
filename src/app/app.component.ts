34af0e161ba402c481fe3d5897fb78ef146f4365
mazimuzi <eemelisalmikunnas@gmail.com>
2024-10-17 11:24:37 +0300

    ?????

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from "./hello-world/hello-world.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent, PageNotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
