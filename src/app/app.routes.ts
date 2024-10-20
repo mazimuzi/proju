d0d664d1ad03ce8cf844df9ddcf7a00bf4ce926b
mazimuzi <eemelisalmikunnas@gmail.com>
2024-10-21 01:46:37 +0300

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 export const routes: Routes = [
     { path: 'hello', component: HelloWorldComponent },
+    { path: '', redirectTo: '/hello', pathMatch: 'full'},
     { path: '**', component: PageNotFoundComponent }
 ];
