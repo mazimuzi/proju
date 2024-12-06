809a588282372739c8e34a6699b7446d7403a290
mazimuzi <eemelisalmikunnas@gmail.com>
2024-12-06 19:24:05 +0200

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from "./hello-world/hello-world.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloWorldComponent, PageNotFoundComponent],
+  imports: [RouterOutlet, HelloWorldComponent, PageNotFoundComponent, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
